f51a62801aade070efe65b53adec08cac4bd5b31
Inna Kirianova <innr@ya.ru>
2022-12-26 19:55:06 +0300

    added code for task63

---
+++ Task63/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+// Первый вариант с count
+// int count = 0;
+// void Numbers(int num)
+// {
+//     if(num == 0)
+//     {
+//         return;
+//     }
+//     count++;
+//     Console.Write($"{count} ");
+//     Numbers(num-1);
+// }
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Numbers(number);
+// Второй вариант без count
+void Numbers(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    Numbers(num - 1);
+    Console.Write($"{num} ");
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Numbers(number);
